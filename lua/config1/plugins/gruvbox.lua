b4cdfe64a1a27be34c6cb9cbe03472ef7cabcd3d
ShubhDPatel <shubh09226@gmail.com>
2023-12-04 23:07:43 -0500

    adfadsf

--- lua/config1/plugins/gruvbox.lua
+++ lua/config1/plugins/gruvbox.lua
@@ -5,7 +5,9 @@ return {
     config = function()
         vim.g.gruvbox_italic_comments = false -- Makes comments not italics
         vim.g.gruvbox_italic_keywords = false -- Makes keywords not italics
-        
+
+        vim.g.gruvbox_theme = { Identifier = { '#ea6962'} }
+       
         vim.cmd([[colorscheme gruvbox-flat]])
     end,
 }
