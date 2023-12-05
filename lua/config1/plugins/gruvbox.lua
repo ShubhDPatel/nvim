63f76fca8e71b64c1777bf1f2e388fb40154d54c
ShubhDPatel <shubh09226@gmail.com>
2023-12-04 21:45:58 -0500

    Changd theme

--- lua/config1/plugins/gruvbox.lua
+++ lua/config1/plugins/gruvbox.lua
@@ -1,8 +1,11 @@
 return {
-    "sainnhe/gruvbox-material",
+    "luisiacc/gruvbox-baby",
     priority = 1000, 
    
     config = function()
-        vim.cmd([[colorscheme gruvbox-material]])
+
+        vim.g.gruvbox_baby_comment_style = "medium", -- Makes it so that comments are not italics
+
+        vim.cmd([[colorscheme gruvbox-baby]])
     end,
 }
