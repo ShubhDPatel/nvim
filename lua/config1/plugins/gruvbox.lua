3d984b68206252769dd7bc2562019130a51975d4
ShubhDPatel <shubh09226@gmail.com>
2023-12-05 12:21:07 -0500

    Change var color

--- lua/config1/plugins/gruvbox.lua
+++ lua/config1/plugins/gruvbox.lua
@@ -6,8 +6,7 @@ return {
         vim.g.gruvbox_italic_comments = false -- Makes comments not italics
         vim.g.gruvbox_italic_keywords = false -- Makes keywords not italics
 
-        vim.cmd[[highlight @variable guifg=#85cae6]]
-
         vim.cmd([[colorscheme gruvbox-flat]])
+        vim.cmd[[highlight TSVariable guifg=#85cae6]]
     end,
 }
