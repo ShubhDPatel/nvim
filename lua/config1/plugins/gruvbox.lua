9609c6fb9dd6d99f4858e852c18d112e4301d89a
ShubhDPatel <shubh09226@gmail.com>
2023-12-04 22:30:20 -0500

    Install treesitter

--- lua/config1/plugins/gruvbox.lua
+++ lua/config1/plugins/gruvbox.lua
@@ -4,6 +4,8 @@ return {
     enabled = true,
     config = function()
         vim.g.gruvbox_italic_comments = false -- Makes comments not italics
+        vim.g.gruvbox_italic_keywords = false -- Makes keywords not italics
+        
         vim.cmd([[colorscheme gruvbox-flat]])
     end,
 }
