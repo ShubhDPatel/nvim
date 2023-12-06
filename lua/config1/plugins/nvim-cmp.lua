b4b7fced591671150bfb3910d8b486d17dd1a00d
ShubhDPatel <shubh09226@gmail.com>
2023-12-05 20:26:20 -0500

    Change some structure

--- lua/config1/plugins/nvim-cmp.lua
+++ lua/config1/plugins/nvim-cmp.lua
@@ -1,14 +1,14 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-            "hrsh7th/cmp-nvim-lsp", -- For lsp servers
-            "hrsh7th/cmp-nvim-lua", -- For vim stuff
-            "hrsh7th/cmp-buffer", -- Buffer completions
-            "hrsh7th/cmp-path", -- Path completions
-            "hrsh7th/cmp-cmdline", -- cmdline completions
-            "saadparwaiz1/cmp_luasnip", -- snippet completions
-            "L3MON4D3/LuaSnip", -- snippet engine
-            "onsails/lspkind.nvim",
+        "hrsh7th/cmp-nvim-lsp", -- For lsp servers
+        "hrsh7th/cmp-nvim-lua", -- For vim stuff
+        "hrsh7th/cmp-buffer", -- Buffer completions
+        "hrsh7th/cmp-path", -- Path completions
+        "hrsh7th/cmp-cmdline", -- cmdline completions
+        "saadparwaiz1/cmp_luasnip", -- snippet completions
+           "L3MON4D3/LuaSnip", -- snippet engine
+        "onsails/lspkind.nvim",
     },
    
     event = {"InsertEnter", "CmdlineEnter"}, -- Load the plugin when entering insert mode
@@ -31,11 +31,11 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
             ["<C-k>"] = cmp.mapping.select_prev_item(),
-		    ["<C-j>"] = cmp.mapping.select_next_item(),
-            ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-            ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+            ["<C-j>"] = cmp.mapping.select_next_item(),
+            -- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+            -- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
             ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.abort(),
+            -- ["<C-e>"] = cmp.mapping.abort(),
             ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources(
@@ -50,16 +50,17 @@ return {
            }
         ),
         formatting = {
-                format = lspkind.cmp_format({
-    mode = "symbol_text",
-    menu = ({
-      buffer = "[Buffer]",
-      nvim_lsp = "[LSP]",
-      luasnip = "[LuaSnip]",
-      nvim_lua = "[Lua]",
-      latex_symbols = "[Latex]",
-    })
-  }),
+            fields = { "kind", "abbr", "menu"},
+            format = lspkind.cmp_format({
+                mode = "symbol_text",
+                menu = ({
+                    luasnip = "[LuaSnip]",
+                    buffer = "[Buffer]",
+                    nvim_lsp = "[LSP]",
+                    nvim_lua = "[Lua]",
+                    latex_symbols = "[Latex]",
+                })
+            }),
         },
     })
 
