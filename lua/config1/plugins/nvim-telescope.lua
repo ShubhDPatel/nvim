2e8b34cacff66a3cca42b29480aff5d7c51cafd5
ShubhDPatel <shubh09226@gmail.com>
2023-12-05 23:01:09 -0500

    Add some icons

--- lua/config1/plugins/nvim-telescope.lua
+++ lua/config1/plugins/nvim-telescope.lua
@@ -8,7 +8,13 @@ return {
         'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     },
     config = function()
-        require('telescope').setup {}
+        require('telescope').setup {
+            defaults = {
+                prompt_prefix = " ",
+                selection_caret = " ",
+                path_display = { "smart" },
+            }
+        }
         
         require('telescope').load_extension('fzf')
 
