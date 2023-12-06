d52992d2da3f778704c2d620b8e781653f386597
ShubhDPatel <shubh09226@gmail.com>
2023-12-06 18:08:43 -0500

    Fully configure harpoon to my needs

--- lua/config1/plugins/harpoon.lua
+++ lua/config1/plugins/harpoon.lua
@@ -6,6 +6,8 @@ return {
         "nvim-lua/plenary.nvim",
     },
     
+    keys = {"<leader>a", "<leader>e"},
+
     config = function()
     local harpoon = require("harpoon")
 
@@ -14,11 +16,11 @@ return {
     -- REQUIRED
 
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+    -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+    -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+    -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
   end,
 }
