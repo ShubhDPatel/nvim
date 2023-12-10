13ff3fddbb021f582ba2b90881f8687b73346246
ShubhDPatel <shubh09226@gmail.com>
2023-12-10 00:49:26 -0500

    null-ls

---
+++ lua/config1/plugins/mason-null-ls.lua
@@ -0,0 +1,19 @@
+return {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-null-ls").setup({
+            ensure_installed = { "stylua", "clang-format" }
+        })
+        require("null-ls").setup({
+        sources = {
+        -- Anything not supported by mason.
+        }
+})
+    end,
+}
--- lua/config1/plugins/null-ls.lua
+++ lua/config1/plugins/null-ls.lua
@@ -1,5 +1,8 @@
 return {
     "jose-elias-alvarez/null-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
     opts = function()
         local null_ls = require("null-ls")
         sources = {
