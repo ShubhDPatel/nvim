816ab9cfe78bcfe88393a7e7cb2a2a9b630e6159
ShubhDPatel <shubh09226@gmail.com>
2023-12-19 17:11:09 -0500

    stuff

--- lua/config1/core/keymaps.lua
+++ lua/config1/core/keymaps.lua
@@ -1,2 +1,10 @@
 vim.keymap.set("n",":ee", "<cmd>NvimTreeToggle<CR>") -- Toggle file explorer on 'ee' key press
 vim.g.mapleader = ' '
+
+-- Already set in the plugin:
+-- vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+-- vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+-- vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+-- vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+-- vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+-- vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
