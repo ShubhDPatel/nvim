b5e1290418ea21e778c0f3b509ebd17aa88a7ae7
ShubhDPatel <shubh09226@gmail.com>
2023-12-29 23:50:13 -0500

    Fix delay to go into command mode

--- lua/config1/core/keymaps.lua
+++ lua/config1/core/keymaps.lua
@@ -1,10 +1,13 @@
-vim.keymap.set("n",":ee", "<cmd>NvimTreeToggle<CR>") -- Toggle file explorer on 'ee' key press
+-- Already set in plugin file:
+-- vim.keymap.set("c", "ee", "NvimTreeToggle<CR>"), -- Toggle file explorer on 'ee' key press
+
+-- Set leader key to space
 vim.g.mapleader = ' '
 
 -- When in terminal, you can press esc to go into normal mode
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
 
--- Already set in the plugin:
+-- Already set in plugin file:
 -- vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
 -- vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
 -- vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
