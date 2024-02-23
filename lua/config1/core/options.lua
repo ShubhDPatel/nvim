d8fbb33eb59aff23a85953e441f32747cca7e04f
ShubhDPatel <shubh09226@gmail.com>
2024-02-23 14:51:16 -0500

    Add support to quick add comments

--- lua/config1/core/options.lua
+++ lua/config1/core/options.lua
@@ -14,13 +14,26 @@ vim.opt.laststatus = 3
 
 -- Set listchars globally
 vim.opt.list = true
-vim.opt.listchars = { space = '·', tab = '> ', extends = '|' }
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.listchars = { space = "·", tab = "> ", extends = "|" }
+vim.opt.clipboard = "unnamedplus"
 
 -- Sets incsearch and hlsearch on
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 -- makes it so whenever : is typed, it doesn't reindent
-vim.cmd [[autocmd FileType cpp setlocal indentkeys-=:]]
-vim.cmd [[autocmd FileType c setlocal indentkeys-=:]]
+vim.cmd([[autocmd FileType cpp setlocal indentkeys-=:]])
+vim.cmd([[autocmd FileType c setlocal indentkeys-=:]])
+
+-- Allows to comment using control + / in visual mode
+vim.cmd([[
+augroup visual_commenting
+    autocmd!
+    autocmd FileType c,cpp,java,rust  let b:comment_symbol = '//'
+    autocmd FileType vim              let b:comment_symbol = '"'
+    autocmd FileType sh,vim,python    let b:comment_symbol = '#'
+    autocmd FileType tex              let b:comment_symbol = '%'
+    autocmd BufEnter * silent! vnoremap <silent> <C-_> :<C-u>keepp '<,'>s@^@\=b:comment_symbol<CR>
+    autocmd BufEnter * silent! exec 'vnoremap <silent> <C-?> :<C-u>keepp ''<,''>s@^' . b:comment_symbol . '@<CR>'
+augroup END
+]])
