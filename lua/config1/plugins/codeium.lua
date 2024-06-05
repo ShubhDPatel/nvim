e39ef858e94cabb03a70e4baf0c758b7b7fae7b1
sdp <Shubh.Patel@keyfactor.com>
2024-06-05 10:48:39 -0400

    Add dependencies to codeium

--- lua/config1/plugins/codeium.lua
+++ lua/config1/plugins/codeium.lua
@@ -1,6 +1,13 @@
 return {
     'Exafunction/codeium.vim',
+
     event = 'InsertEnter',
+
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+
     config = function()
         vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
         vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
